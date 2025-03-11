8a2b0454f8873fbe2efa31a1122da48823f626ac
Sunnyboy037 <qkqehenr37@gmail.com>
2025-03-11 14:42:04 +0900

    20250311

---
+++ src/com/grepp/library/a_wrapper/A_wrapper.java
@@ -0,0 +1,18 @@
+package com.grepp.library.a_wrapper;
+
+public class A_wrapper {
+
+    private boolean activated;
+    private Boolean wActivated;
+    private int num;
+    private Integer wNum;
+    private char ch;
+    private Character wCh;
+    private double dNum;
+    private Double wDNum;
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/com/grepp/library/b_string/A_concat.java
@@ -0,0 +1,11 @@
+package com.grepp.library.b_string;
+
+public class A_concat {
+
+    public static void main(String[] args) {
+
+        String java = "JAVA";
+        String java2 = "JAVA";
+    }
+
+}
